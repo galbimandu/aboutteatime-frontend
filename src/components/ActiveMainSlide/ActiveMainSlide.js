e05babe78dd4443abec936b359765e13e181f769
PARKJONGSUN88 <parksunl@nate.com>
2020-05-30 23:37:08 +0900

    [ADD] Action 페이지 추가 - 스크롤 위치에 따른 동적 view 페이지 구현중 - 페이지 분석완료: 고정된 X축 슬라이드, 움직이는 Y축 슬라이드, 우측 fixed nav바 존재 - 스크롤 위치값 추가, 스크롤 위치값 자동변경 및 위치값에 따른 slide 이벤트 구현 - Y축 휠이벤트나 클릭이벤트로 자동실행, 일시정지, 재실행 함수 구현 완료. CSS, img소스 작업중 - X축 레이아웃 완료, 함수 작업예정 - fixed nav바 레이아웃 완료, 함수 및 CSS 작업 예정

---
+++ src/components/ActiveMainSlide/ActiveMainSlide.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./ActiveMainSlide.scss";
+
+class ActiveMainSlide extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="activeMainSlide">
+        <div className="test">1</div>
+        <div className="test">2</div>
+        <div className="test">3</div>
+        <div className="test">4</div>
+        <div className="test">5</div>
+        <div className="test">6</div>
+        <div className="test">7</div>
+        <div className="test">8</div>
+        <div className="test">9</div>
+        <div className="test">10</div>
+        <div className="test">11</div>
+        <div className="test">12</div>
+        <div className="test">13</div>
+        <div className="test">14</div>
+        <div className="test">15</div>
+        <div className="test">16</div>
+        <div className="test">17</div>
+        <div className="test">18</div>
+        <div className="test">19</div>
+        <div className="test">20</div>
+      </div>
+    );
+  }
+}
+
+export default ActiveMainSlide;
