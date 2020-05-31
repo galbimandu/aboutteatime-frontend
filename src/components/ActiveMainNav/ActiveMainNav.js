abd0064d6c63e6250ec9f4e8ba73d86be29891b4
PARKJONGSUN88 <parksunl@nate.com>
2020-05-31 17:57:48 +0900

    [ADD]Action페이지 기능 추가 - Y축 스크롤값에 따라 transform되는 이미지 구현완료 - X축, Y축 각 slide 기능 구현 완료   - 스크롤값별 이벤트 구현 완료(source, 기능함수, runtime조정, 스크롤값 조정) - nav바 바로가기기능 완료 - nav바 style 작업중

--- src/components/ActiveMainNav/ActiveMainNav.js
+++ src/components/ActiveMainNav/ActiveMainNav.js
@@ -4,12 +4,85 @@ import "./ActiveMainNav.scss";
 class ActiveMainNav extends Component {
   constructor() {
     super();
+
+    this.state = {
+      part1: false,
+      part2: false,
+      part3: false,
+    };
+  }
+
+  navPart1Func() {
+    this.props.navDirectGoFunc(1);
+  }
+  navPart2Func() {
+    this.props.navDirectGoFunc(2);
+  }
+  navPart3Func() {
+    this.props.navDirectGoFunc(3);
   }
 
   render() {
+    // console.log(this.state.part1);
     return (
       <div className="activeMainNav">
-        <div></div>네비게이션 준비중
+        <div className="navButton">
+          <div
+            className={
+              this.state.part1 ? "navCommon part1 active" : "navCommon part1"
+            }
+            onClick={this.navPart1Func.bind(this)}
+            onMouseOver={() => this.setState({ part1: true })}
+            onMouseLeave={() => this.setState({ part1: false })}
+          >
+            <div
+              className={
+                this.state.part1
+                  ? "textCommon textActive"
+                  : "textCommon textHidden"
+              }
+            >
+              <span>일구다</span>
+            </div>
+          </div>
+          <div
+            className={
+              this.state.part2 ? "navCommon part2 active" : "navCommon part2"
+            }
+            onClick={this.navPart2Func.bind(this)}
+            onMouseOver={() => this.setState({ part2: true })}
+            onMouseLeave={() => this.setState({ part2: false })}
+          >
+            <div
+              className={
+                this.state.part2
+                  ? "textCommon textActive"
+                  : "textCommon textHidden"
+              }
+            >
+              <span>가꾸다</span>
+            </div>
+          </div>
+          <div
+            className={
+              this.state.part3 ? "navCommon part3 active" : "navCommon part3"
+            }
+            onClick={this.navPart3Func.bind(this)}
+            onMouseOver={() => this.setState({ part3: true })}
+            onMouseLeave={() => this.setState({ part3: false })}
+          >
+            <div
+              className={
+                this.state.part3
+                  ? "textCommon textActive"
+                  : "textCommon textHidden"
+              }
+            >
+              <span>즐기다</span>
+            </div>
+          </div>
+        </div>
+        {/* <img src={require("../../images/activeMain/nav.png")} alt="sample" /> */}
       </div>
     );
   }
